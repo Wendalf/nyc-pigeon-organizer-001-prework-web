9e2a9543cdb78d701d3286c72e7d398367964eef
Wendalf <guangw.xu@gmail.com>
2016-05-04 18:36:47 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
+  pigeon_list = {}
+  pigeon_data.each do |attribute, value|
+    value.each do |key, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attribute] ||= []
+        pigeon_list[pigeon][attribute] << key.to_s 
+      end
+    end
+  end
+  pigeon_list
 end
