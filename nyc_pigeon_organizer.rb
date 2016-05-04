f7c8514c46f7f6ba1a7a977fb808162a5571723b
Wendalf <guangw.xu@gmail.com>
2016-05-04 19:00:16 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,25 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |attribute, value|
     value.each do |key, pigeons|
       pigeons.each do |pigeon|
+        # if pigeon_list.has_key?(pigeon)
+        #   if pigeon_list[pigeon].has_key?(attribute)
+        #     pigeon_list[pigeon][attribute] << key.to_s
+        #   else
+        #     pigeon_list[pigeon][attribute] = []
+        #     pigeon_list[pigeon][attribute] << key.to_s
+        #   end
+        # else
+        #   pigeon_list[pigeon] = {}
+        #   if pigeon_list[pigeon].has_key?(attribute)
+        #     pigeon_list[pigeon][attribute] << key.to_s
+        #   else
+        #     pigeon_list[pigeon][attribute] = []
+        #     pigeon_list[pigeon][attribute] << key.to_s
+        #   end
+        # end
         pigeon_list[pigeon] ||= {}
         pigeon_list[pigeon][attribute] ||= []
-        pigeon_list[pigeon][attribute] << key.to_s 
+        pigeon_list[pigeon][attribute] << key.to_s
       end
     end
   end
